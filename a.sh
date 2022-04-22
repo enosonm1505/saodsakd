701ff950cbb079aa6241a16e1ece4df1cbbc7f48
enosonm1505 <91190634+enosonm1505@users.noreply.github.com>
2022-04-22 11:49:55 +0530

    a.sh

---
+++ a.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+a="evalo_"$(date +%Y%m%d"_"%H%M%S)".sql.gz"
+echo $a
+da=$(date +%y%m%d)
+echo $da
+mysqldump -u root -p'dAFD8$237Ecvg@OzDZ0Dn0wfZ@HH' evalo | gzip > /root/bak/172.31.13.99/172.31.13.99/"$(date +%y%m%d)"/$a
+aws s3 sync /root/bak/172.31.13.99/ s3://sms-mysql-backup-evalo/
+asd=$(ls /root/bak/172.31.13.99/172.31.13.99/$da | grep $a)
+echo $asd
+if gzip -t /root/bak/172.31.13.99/172.31.13.99/"$da"/"$asd" ; then
+    echo 'file is ok'
+    python3 a.py
+else
+    echo 'file is corrupt'
+    python3 e.py
+fi
